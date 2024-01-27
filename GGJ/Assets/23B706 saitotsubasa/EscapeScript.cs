a8313316b25962437759f0b1dde41815e2e4fdc4
Saito197-shohoku <23B706@shohoku.ac.jp>
2024-01-27 15:51:41 +0900

    UIのボタンを作成し、それに対応するスクリプトを設定した

---
+++ GGJ/Assets/23B706 saitotsubasa/EscapeScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EscapeScript : MonoBehaviour
+{
+    
+    //ボタンが押された時に呼ばれる関数
+    public void OnClick()
+    {
+        Debug.Log("Escape!");
+    }
+}
